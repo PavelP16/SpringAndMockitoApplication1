59766a81b1aa22809c3e570c6babe9449a59e550
Pavel <pavelpulle1@gmail.co>
2024-09-13 22:30:17 +0300

    Initial commit

---
+++ SpringAndMockitoApplication1/src/main/java/com/example/SpringAndMockitoApplication1/SpringAndMockitoApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringAndMockitoApplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMockitoApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMockitoApplication1Application.class, args);
+	}
+
+}
---
+++ SpringAndMockitoApplication1/src/test/java/com/example/SpringAndMockitoApplication1/SpringAndMockitoApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAndMockitoApplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndMockitoApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
